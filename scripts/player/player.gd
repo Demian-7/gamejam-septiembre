4864e54d95b307eadeaed0a7154b5c6553e4c65e
Demian <demianlozanoriedel@gmail.com>
2024-09-03 23:54:49 -0300

    Player Script - Movement added

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,2 +1,22 @@
 class_name Player
 extends CharacterBody2D
+
+@export var speed : float = 500
+var target_direction := Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	# Vector de movimiento
+	target_direction = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+	# Vector de dirección normalizado para mantener la misma velocidad en todas las direcciones
+	if target_direction.length() > 0:
+		target_direction = target_direction.normalized()
+
+	
+	velocity = target_direction * speed
+	
+	move_and_slide()
